769d6c3a529b2b2787f28717a6c856931f0ab56a
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-07-06 20:53:14 +0900

    PrismaをインポートしてデータをDBから取得してくる処理を追加

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,6 +1,13 @@
 import { categories } from "@/_mock/categories";
+import { prisma } from "@/lib/prisma";
 
 export async function GET() {
+
+  const categoriesFromDb = await prisma.category.findMany();
+  categoriesFromDb.map(c => {
+    console.log(c.id + "," + c.name + "," + c.label + "," + c.description + "," + c.imageUrl);
+  });
+
   // 🚧: DBに接続しレコードを取得する
   return Response.json({ categories });
 }
