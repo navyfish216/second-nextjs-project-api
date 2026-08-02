89d61c66652fed301eea10f1e16c095a134a25d6
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-08-02 20:53:15 +0900

    トークン作成日時を追加し、古いトークンを削除する処理を追加

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,23 +1,39 @@
+import { isBefore, add } from "date-fns";
+
 type User = {
     userId: string;
+    addDate: Date;
 }
 
 const userMap = new Map<string, User>();
 
-export function setUserMap(token: string, userId: string) {
-  console.log("setUserMap");
-  userMap.set(token, {userId: userId});
+function cleanUpAndDebug() {
+  const comparisonDate = add(new Date(), {
+    minutes: -15,
+  });
 
   userMap.forEach((value, key) => {
-    console.log(key, value.userId);
+    if(isBefore(value.addDate, comparisonDate)) {
+      console.log(`delete: ${key} ${value.userId}`);
+      userMap.delete(key);
+    } else {
+      console.log(`${key} ${value.userId}`);
+    }
   });
+}
+
+export function setUserMap(token: string, userId: string) {
+  console.log("setUserMap");
+  userMap.set(token, {
+    userId: userId, 
+    addDate: new Date()
+  });
+  cleanUpAndDebug();
 }
 
 export function getUserMap(token: string): User | undefined {
   console.log("getUserMap");
-  userMap.forEach((value, key) => {
-    console.log(key, value.userId);
-  });
+  cleanUpAndDebug();
   const user = userMap.get(token);
   console.log(`getUserMap user ${user?.userId}`);
 
