1286d4977fc3e63879e85185414853651770dcee
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-08-12 18:57:26 +0900

    サーバー側のトークン保持期間設定箇所をを定数→envに変更

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,6 +1,11 @@
 import { isBefore, add } from "date-fns";
 import { type User } from "@/type";
 
+// リフレッシュトークン有効期間
+const refreshTokenMaxAge: number = -Number(process.env.REFRESH_TOKEN_MAX_AGE);
+// アクセストークン有効期間
+const accessTokenMaxAge: number = -Number(process.env.ACCESS_TOKEN_MAX_AGE);
+
 const refreshTokenMap = new Map<string, User>();
 const accessTokenMap = new Map<string, User>();
 
@@ -21,12 +26,12 @@ export function setRefreshTokenMap(token: string, userId: string) {
     userId: userId, 
     addDate: new Date()
   });
-  cleanUpAndDebug(refreshTokenMap, add(new Date(), {days: -7}));
+  cleanUpAndDebug(refreshTokenMap, add(new Date(), {days: refreshTokenMaxAge}));
 }
 
 export function getRefreshTokenMap(token: string): User | undefined {
   console.log("getRefreshTokenMap");
-  cleanUpAndDebug(refreshTokenMap, add(new Date(), {days: -7}));
+  cleanUpAndDebug(refreshTokenMap, add(new Date(), {days: refreshTokenMaxAge}));
   const user = refreshTokenMap.get(token);
   console.log(`getRefreshTokenMap user ${user?.userId}`);
 
@@ -39,12 +44,12 @@ export function setAccessTokenMap(token: string, userId: string) {
     userId: userId, 
     addDate: new Date()
   });
-  cleanUpAndDebug(accessTokenMap, add(new Date(), {minutes: -15}));
+  cleanUpAndDebug(accessTokenMap, add(new Date(), {minutes: accessTokenMaxAge}));
 }
 
 export function getAccessTokenMap(token: string): User | undefined {
   console.log("getAccessTokenMap");
-  cleanUpAndDebug(accessTokenMap, add(new Date(), {minutes: -15}));
+  cleanUpAndDebug(accessTokenMap, add(new Date(), {minutes: accessTokenMaxAge}));
   const user = accessTokenMap.get(token);
   console.log(`getAccessTokenMap user ${user?.userId}`);
 
